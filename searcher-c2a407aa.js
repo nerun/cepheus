8f2e6063b037398a259d0d4b0af1791d588c55a2
nerun <danieldiasr@gmail.com>
2026-01-07 14:40:06 -0300

    Adicionado manifesto ao SUMMARY.md

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ee3da493.js',
+                path_to_root + 'searchindex-d68c0bf6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
